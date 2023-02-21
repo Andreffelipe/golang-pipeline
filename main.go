dd1c85b3dd813c10ff6d292841b826684b1dc58b
Andreffelipe <felipe.andre35@yahoo.com.br>
2023-02-21 14:52:12 -0300

    feat: implementing the main file

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Hello() string {
+	return "Hello, welcome to gpher"
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/api/hello", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(Hello()))
+	}))
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: mux,
+	}
+	fmt.Println("Server listening on http://localhost:3000")
+	log.Fatal(server.ListenAndServe())
+}
